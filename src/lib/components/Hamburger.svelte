67e72b442ec1828d17ac2e110f1aea77cfede259
maxpiano <max@bilov.de>
2022-01-22 15:31:31 +0100

    finished very hacky hamburger button

--- src/lib/components/Hamburger.svelte
+++ src/lib/components/Hamburger.svelte
@@ -1,3 +1,37 @@
-<div>
-  <span />
+<script lang="ts">
+  let hovering: boolean;
+  let open: boolean = false;
+</script>
+
+<div
+  class="cursor-pointer w-10 h-8 flex flex-col justify-around items-end transition-all "
+  class:open
+  on:mouseenter={() => (hovering = true)}
+  on:mouseleave={() => (hovering = false)}
+  on:click={() => (open = !open)}
+>
+  <span class={hovering ? 'w-full' : 'w-5/6'} />
+  <span class={hovering ? 'w-full' : 'w-3/5'} />
+  <span class="w-full" />
 </div>
+
+<style lang="postcss">
+  span {
+    @apply bg-white transition-all duration-200 ease-in-out origin-left;
+    height: 1px;
+  }
+  .open span:nth-child(1) {
+    width: 100%;
+    transform: translate(6px, -3.5px) rotate(45deg);
+  }
+  .open span:nth-child(2) {
+    opacity: 0;
+  }
+  .open span:nth-child(3) {
+    width: 100%;
+    transform: translate(6px, 3.5px) rotate(-45deg);
+  }
+  .open:hover {
+    transform: scale(1.2);
+  }
+</style>
--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -5,13 +5,22 @@
 </script>
 
 <div
-  class="pt-72 h-screen bg-[url('/images/millerbg.jpg')] bg-cover bg-center flex items-center justify-center flex-col gap-40"
+  class="pt-64 h-screen bg-[url('/images/millerbg.jpg')] bg-cover bg-center flex items-center justify-center flex-col gap-32"
 >
-  <div class="flex gap-4 tracking-[.25em] font-oswald text-4xl uppercase text-amber-600">
-    <span class="font-bold">beautifully</span>
-    <span class="font-extralight">designed</span>
-    <span class="font-extralight text-white">music</span>
-    <span class="font-bold text-white">theme</span>
+  <img src="/logos/sixtone.png" alt="" class="absolute top-16 opacity-5 w-full" />
+  <div
+    class="flex flex-col items-end gap-4 tracking-[.25em] font-oswald text-4xl uppercase text-amber-600"
+  >
+    <div class="flex gap-4">
+      <span class="font-bold">music</span>
+      <span class="font-extralight">is</span>
+      <span class="font-extralight ">what</span>
+    </div>
+    <span class="font-extralight text-white self-start">feelings</span>
+    <div class="flex gap-4">
+      <span class="font-extralight ">sound</span>
+      <span class="font-bold">like</span>
+    </div>
   </div>
   <ScrollIndicator />
 </div>
