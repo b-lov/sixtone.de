8cfba405db87c0d8d0a78fc45bcf5b106bb7b7c7
maxpiano <max@bilov.de>
2022-01-23 13:00:06 +0100

    added navmenu component

---
+++ src/lib/sections/NavMenu.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  let open: boolean = false;
+</script>
+
+{#if open}
+  <div class="fixed bg-black w-screen h-screen z-10 bg-opacity-80">blablabla</div>
+{/if}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,13 @@
 <script>
   import Footer from '$lib/sections/Footer.svelte';
   import Header from '$lib/sections/Header.svelte';
+  import NavMenu from '$lib/sections/NavMenu.svelte';
 
   import '../app.css';
 </script>
 
 <div class="bg-neutral-800">
+  <NavMenu />
   <Header />
   <slot />
   <Footer />
