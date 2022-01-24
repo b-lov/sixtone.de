a69856a09f6ea04d07e582d6345e23390f159f8d
maxpiano <max@bilov.de>
2022-01-24 09:31:30 +0100

    menu slides

--- src/lib/sections/NavMenu.svelte
+++ src/lib/sections/NavMenu.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+  import { slide, fade, fly } from 'svelte/transition';
+
   export let open: boolean;
+  let clientWidth: number;
 </script>
 
 {#if open}
-  <div class="fixed bg-black w-screen h-screen bg-opacity-80 z-10 text-white">blablabla</div>
+  <div
+    bind:clientWidth
+    transition:fly={{ x: clientWidth }}
+    class="flex justify-center items-center fixed bg-black w-screen h-screen bg-opacity-80 z-10 text-white"
+  >
+    bla
+  </div>
 {/if}
