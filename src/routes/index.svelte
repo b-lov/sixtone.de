2b802c1de2b90544c3d45d8410427a75fcb68609
maxpiano <max@bilov.de>
2022-02-02 22:28:37 +0100

    accidentally working background parallax

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,17 +7,31 @@
 </script>
 
 <svelte:window bind:scrollY />
-<div
-  class="fixed h-screen w-screen -z-10 bg-cover bg-right bg-neutral-800/90 bg-blend-multiply "
-  style="background-image: url(/images/bg1.jpg); transform: translateY(-{scrollY / 3}px);"
-/>
+<div style="transform: translateY(-{scrollY / 2}px);" class="fixed -z-10 w-screen">
+  <div
+    class="h-screen bg-cover bg-right bg-neutral-800/90 bg-blend-multiply "
+    style="background-image: url(/images/bg1.jpg);"
+  />
+  <div
+    class="h-screen bg-cover bg-right bg-neutral-800/90 bg-blend-multiply "
+    style="background-image: url(/images/bg2.jpg);"
+  />
+  <div
+    class="h-screen bg-cover bg-right bg-neutral-800/90 bg-blend-multiply "
+    style="background-image: url(/images/bg3.jpg);"
+  />
+  <div
+    class="h-screen bg-cover bg-right bg-neutral-800/90 bg-blend-multiply "
+    style="background-image: url(/images/bg4.jpg);"
+  />
+</div>
 {#each $pageSections as section, i}
   {#if section === 'Home'}
     <Home />
   {:else if section === 'Team'}
     <Team />
   {:else}
-    <div id={section} class="h-screen flex  justify-end  {i % 2 && 'bg-neutral-900'}">
+    <div id={section} class="h-screen flex justify-end  {i % 2 && 'bg-neutral-900'}">
       <h1
         class="text-accent text-6xl uppercase tracking-[.3em] font-bold
         transition-all duration-300 mt-6 opacity-60 {$navMenuOpen ? 'mr-72' : 'mr-6'}"
