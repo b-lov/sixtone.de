da4a6825e3883369858d4915ab40356dccf6d290
maxpiano <max@bilov.de>
2022-03-06 23:45:59 +0100

    added feedback component

---
+++ src/lib/components/Sections/Feedback/SlideAuthor.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let picture;
+  export let name;
+</script>
+
+<div class="flex flex-col items-center gap-6">
+  <img src={picture} alt="" class="w-24 h-24 object-cover rounded-full" />
+  <div class="text-white font-oswald text-xl font-light">{name}</div>
+</div>
---
+++ src/lib/components/Sections/Feedback/SlideText.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<div class="text-neutral-400 italic h-60 flex items-center">
+  {text}
+</div>
