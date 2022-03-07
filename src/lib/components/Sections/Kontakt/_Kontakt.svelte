382ff8488662bd9c5a6d4390023d0e2abb1068b2
maxpiano <max@bilov.de>
2022-03-07 14:51:14 +0100

    different backgroujnd

--- src/lib/components/Sections/Kontakt/_Kontakt.svelte
+++ src/lib/components/Sections/Kontakt/_Kontakt.svelte
@@ -4,7 +4,7 @@
 
 <form
   class="
-    flex-1 flex flex-col justify-center px-12 gap-8 bg-[url('images/contactbg2.jpg')]
+    flex-1 flex flex-col justify-center px-12 gap-8 bg-[url('images/contactbg.jpg')]
     bg-cover bg-neutral-900/80 bg-blend-multiply bg-fixed items-start 
   "
 >
