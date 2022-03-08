e78984629f4992221b5756ce78e0cba5374d559e
maxpiano <max@bilov.de>
2022-03-08 21:03:11 +0100

    added tracks object

---
+++ src/lib/components/Sections/Sounds/tracks.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: 'All that she wants',
+    file: 'all-that.mp3'
+  },
+  {
+    name: 'Euphoria',
+    file: 'euphoria.mp3'
+  },
+  {
+    name: 'Fly me to the moon',
+    file: 'fly-me.mp3'
+  },
+  {
+    name: 'Mix',
+    file: 'mix.m4a'
+  }
+];
