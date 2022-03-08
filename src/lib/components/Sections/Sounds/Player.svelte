151bd12cddc1aa7e92d406b5297da0cb3c8c1297
maxpiano <max@bilov.de>
2022-03-08 21:02:50 +0100

    added player component

---
+++ src/lib/components/Sections/Sounds/Player.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import ProgressBar from './ProgressBar.svelte';
+
+  export let name;
+</script>
+
+<div class="bg-neutral-800 py-4 px-6 flex flex-col gap-4">
+  <h1 class="text-neutral-400 font-light">{name}</h1>
+  <div class="flex">
+    <ProgressBar />
+  </div>
+</div>
