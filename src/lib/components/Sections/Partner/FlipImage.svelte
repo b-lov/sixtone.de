12c46bccbcd9539f183652adc70049cab2e213e1
maxpiano <max@bilov.de>
2022-03-08 01:03:18 +0100

    added partner components

---
+++ src/lib/components/Sections/Partner/FlipImage.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let picture: string;
+  export let name: string;
+  export let flipped: boolean;
+</script>
+
+<div class="scene">
+  <div class="card" class:flipped>
+    <img src="images/partner/{picture}" alt={name} class="front" />
+    <img src="images/partner/{picture}" alt={name} class="back" />
+  </div>
+</div>
+
+<style lang="postcss">
+  .scene {
+    perspective: 1000px;
+  }
+  .card {
+    @apply w-48 h-48 relative;
+    transition: transform 0.7s;
+    transform-style: preserve-3d;
+  }
+  .flipped {
+    transform: rotateY(180deg);
+  }
+  .front,
+  .back {
+    @apply absolute h-full w-full object-cover rounded-full;
+    backface-visibility: hidden;
+  }
+  .front {
+    filter: grayscale(100%);
+  }
+  .back {
+    transform: rotateY(180deg);
+  }
+</style>
---
+++ src/lib/components/Sections/Partner/PartnerCard.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Icons from '$lib/components/Icons.svelte';
+  import FlipImage from './FlipImage.svelte';
+  export let name;
+  export let picture;
+  export let link;
+  let flipped = false;
+</script>
+
+<a href={link} target="_blank" class="mx-auto">
+  <div
+    class="flex flex-col gap-6 items-center"
+    on:mouseenter={() => (flipped = true)}
+    on:mouseleave={() => (flipped = false)}
+  >
+    <FlipImage {picture} {name} {flipped} />
+    <h1
+      class="
+        {flipped ? 'text-accent' : 'text-neutral-300'}
+        font-oswald uppercase tracking-wide transition duration-300 font-light
+      "
+    >
+      {name}
+      <Icons
+        external_link
+        size={0.8}
+        class="
+          {flipped ? 'fill-accent' : 'fill-neutral-300'}
+          inline-block mb-2 ml-0 transition duration-300
+        "
+      />
+    </h1>
+  </div>
+</a>
