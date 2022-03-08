89cd6a516084b4b89a36a4fc402e92435d043153
maxpiano <max@bilov.de>
2022-03-08 13:30:57 +0100

    created sounds component

---
+++ src/lib/components/Sections/Sounds/_Sounds.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  //
+</script>
+
+<div class="text-white flex items-center justify-center px-16">
+  <div class="w-full grid grid-cols-5 gap-8">
+    <div class="col-span-2">
+      <img src="images/music.jpg" class="w-full h-full object-cover object-right" alt="" />
+    </div>
+    <div class="col-span-3 flex flex-col gap-1">
+      <!--  -->
+      <div class="bg-green-50 h-24 border border-dotted border-red-500" />
+      <div class="bg-green-50 h-24 border border-dotted border-red-500" />
+      <div class="bg-green-50 h-24 border border-dotted border-red-500" />
+      <div class="bg-green-50 h-24 border border-dotted border-red-500" />
+    </div>
+  </div>
+</div>
