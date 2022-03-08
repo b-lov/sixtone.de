abf56fd0e1e0fb21736c28be53afccad985358ba
maxpiano <max@bilov.de>
2022-03-08 21:02:59 +0100

    added progress bar component

---
+++ src/lib/components/Sections/Sounds/ProgressBar.svelte
@@ -0,0 +1,11 @@
+<div class="w-full rounded-full bg-neutral-700">
+  <div
+    class="
+          bg-accent text-xs text-neutral-300 text-center p-0.5 leading-none
+          rounded-full
+        "
+    style="width: 45%"
+  >
+    0:58
+  </div>
+</div>
