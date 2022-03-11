5376413c85cf0fd04db3dd7a01ee6a1754642db7
maxpiano <max@bilov.de>
2022-03-12 00:04:44 +0100

    full width for home component

---
+++ src/lib/components/Sections/News/NewsCard.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Button from '$lib/components/Button.svelte';
+
+  export let newsItem: {
+    picture: string;
+    title: string;
+    datum: string;
+    author: string;
+    text: string;
+  };
+
+  const { picture, title, datum, author, text } = newsItem;
+</script>
+
+<div style="background-image: url({picture});" class="shadow-lg bg-top bg-cover">
+  <div
+    class="
+          w-full h-full flex flex-col justify-center items-center p-8 bg-neutral-800
+          bg-opacity-80 text-neutral-400 gap-8
+        "
+  >
+    <div class="flex flex-col items-center gap-2">
+      <a href="#blank" on:click|preventDefault>
+        <h1
+          class="font-oswald uppercase text-2xl font-light text-white hover:text-accent transition"
+        >
+          {title}
+        </h1>
+      </a>
+      <div class="italic font-light text-sm">{datum} von {author}</div>
+    </div>
+    <div class="text-center">{text}</div>
+    <Button text="mehr lesen" />
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
   import Home from '$lib/components/Sections/Home/_Home.svelte';
-  import SectionHeading from '$lib/components/Sections/SectionHeading.svelte';
-  import Team from '$lib/components/Sections/Team/_Team.svelte';
-  import Gallery from '$lib/components/Sections/Gallery/_Gallery.svelte';
+  import Sounds from '$lib/components/Sections/Sounds/_Sounds.svelte';
   import Events from '$lib/components/Sections/Events/_Events.svelte';
+  import Gallerie from '$lib/components/Sections/Gallerie/_Gallerie.svelte';
   import News from '$lib/components/Sections/News/_News.svelte';
   import Feedback from '$lib/components/Sections/Feedback/_Feedback.svelte';
+  import Partner from '$lib/components/Sections/Partner/_Partner.svelte';
   import Kontakt from '$lib/components/Sections/Kontakt/_Kontakt.svelte';
 
   import { pageSections, currentSection } from '$lib/stores';
   import { useViewport } from '$lib/actions';
-  import Partner from '$lib/components/Sections/Partner/_Partner.svelte';
-  import Sounds from '$lib/components/Sections/Sounds/_Sounds.svelte';
 </script>
 
 <!-- TODO: make better use of IntersectionObserver to observe all sections with one instance. -->
@@ -20,29 +18,24 @@
     use:useViewport={{ rootMargin: '-50%' }}
     on:enterViewport={({ detail }) => currentSection.set(detail.target.id)}
     id={section}
-    class="min-h-screen flex flex-col scroll-mt-16"
+    class="scroll-mt-16"
   >
     {#if section === 'Home'}
       <Home />
-    {:else}
-      <SectionHeading {section} />
-      {#if section === 'Team'}
-        <Team />
-      {:else if section === 'Events'}
-        <Events />
-      {:else if section === 'Gallerie'}
-        <Gallery />
-      {:else if section === 'Feedback'}
-        <Feedback />
-      {:else if section === 'Kontakt'}
-        <Kontakt />
-      {:else if section === 'News'}
-        <News />
-      {:else if section === 'Partner'}
-        <Partner />
-      {:else if section === 'Sounds'}
-        <Sounds />
-      {/if}
+    {:else if section === 'Sounds'}
+      <Sounds />
+    {:else if section === 'Events'}
+      <Events />
+    {:else if section === 'Gallerie'}
+      <Gallerie />
+    {:else if section === 'News'}
+      <News />
+    {:else if section === 'Feedback'}
+      <Feedback />
+    {:else if section === 'Partner'}
+      <Partner />
+    {:else if section === 'Kontakt'}
+      <Kontakt />
     {/if}
   </section>
 {/each}
