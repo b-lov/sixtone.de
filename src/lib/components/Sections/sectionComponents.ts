bdb886476c8f138e60dc796d26f911e63d718525
maxpiano <max@bilov.de>
2022-03-12 01:27:39 +0100

    added sectioncomponents array

---
+++ src/lib/components/Sections/sectionComponents.ts
@@ -0,0 +1,43 @@
+import Home from './Home/_Home.svelte';
+import Sounds from './Sounds/_Sounds.svelte';
+import Events from './Events/_Events.svelte';
+import Gallerie from './Gallerie/_Gallerie.svelte';
+import News from './News/_News.svelte';
+import Feedback from './Feedback/_Feedback.svelte';
+import Partner from './Partner/_Partner.svelte';
+import Kontakt from './Kontakt/_Kontakt.svelte';
+
+export default [
+  {
+    name: 'Home',
+    component: Home
+  },
+  {
+    name: 'Sounds',
+    component: Sounds
+  },
+  {
+    name: 'Events',
+    component: Events
+  },
+  {
+    name: 'Gallerie',
+    component: Gallerie
+  },
+  {
+    name: 'News',
+    component: News
+  },
+  {
+    name: 'Feedback',
+    component: Feedback
+  },
+  {
+    name: 'Partner',
+    component: Partner
+  },
+  {
+    name: 'Kontakt',
+    component: Kontakt
+  }
+];
