6a673cda5d49aac8bdc1bd539ddac307003f87b3
maxpiano <max@bilov.de>
2022-03-13 21:27:26 +0100

    better bg stamp without dummy element

--- src/lib/components/Sections/Home/BgStamp.svelte
+++ src/lib/components/Sections/Home/BgStamp.svelte
@@ -2,13 +2,12 @@
   const name: string = 'sixtone';
 </script>
 
-<div class="absolute h-full w-full top-0 overflow-hidden flex flex-col justify-around items-center">
-  <div class="flex justify-between items-center w-[107%]">
-    {#each [...name] as item}
-      <span class="text-white/10 text-[12rem] font-extrabold uppercase select-none cursor-default"
-        >{item}</span
-      >
-    {/each}
-  </div>
-  <div class="dummy" />
+<div class="absolute opacity-[0.12] top-[25%] flex justify-between items-center w-[107%]">
+  {#each [...name] as item}
+    <span
+      class="text-white text-[12rem] font-extrabold uppercase select-none cursor-default leading-none"
+    >
+      {item}
+    </span>
+  {/each}
 </div>
