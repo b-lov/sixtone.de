3bbd716f003bd0edcce88d184b985c1da6a14463
maxpiano <max@bilov.de>
2022-03-13 16:18:11 +0100

    better dynamic background (still sucks)

--- src/lib/components/Sections/DynamicBackground.svelte
+++ src/lib/components/Sections/DynamicBackground.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
   export let image: string;
-  export let opacity: number = 0.7;
+  export let opacity: number = 0.6;
   let dotOverlay: string =
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAABRJREFUGFdjZGBg+M/AwMDIyAAFAA4pAQP4L554AAAAAElFTkSuQmCC';
 </script>
 
-<div class="absolute w-full h-full top-0">
-  <div class="relative w-full h-full overflow-hidden">
-    <div
-      class="absolute w-full h-full bg-cover bg-fixed bg-center scale-110"
-      style="background-image: url(/images/backgrounds/{image}.jpg);"
-    />
-    <div class="absolute h-full w-full bg-neutral-900" style="opacity: {opacity};" />
-    <div class="absolute h-full w-full" style="background-image: url({dotOverlay});" />
-  </div>
-</div>
+<!-- on:mouseenter={() => console.log('hello worlds')} -->
+<div
+  class="absolute w-full h-full bg-cover bg-center bg-fixed"
+  style="background-image: url(images/backgrounds/{image}.jpg);"
+/>
+<div class="absolute h-full w-full bg-neutral-900" style="opacity: {opacity};" />
+<!-- TODO: dot overlay with better performance:
+https://css-tricks.com/snippets/svg/svg-patterns/ -->
+<div class="absolute h-full w-full" style="background-image: url({dotOverlay});" />
