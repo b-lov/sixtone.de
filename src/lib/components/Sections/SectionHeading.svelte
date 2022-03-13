4ffdea7753eab846b94805518c711dc0ccef3708
maxpiano <max@bilov.de>
2022-03-13 12:08:03 +0100

    added all new icons for sections

--- src/lib/components/Sections/SectionHeading.svelte
+++ src/lib/components/Sections/SectionHeading.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <div class="py-16 text-neutral-300 text-center relative">
-  <div class="flex items-center justify-center">
-    <Icons name="sounds" size={2} class="stroke-2 stroke-neutral-300" />
-    <h1 class="text-5xl tracking-widest font-medium first-letter:text-accent p-2">
+  <div class="flex items-center justify-center gap-2">
+    <Icons {name} size={2} class="stroke-1 stroke-neutral-300 fill-[none]" />
+    <h1 class="text-5xl tracking-widest font-semibold first-letter:text-accent p-2">
       {name}
     </h1>
   </div>
