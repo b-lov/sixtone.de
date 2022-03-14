9fe15f0a436323f0c6b43239a14b638c11153e1d
maxpiano <max@bilov.de>
2022-03-14 15:01:10 +0100

    added band icon

--- src/lib/components/Icons.svelte
+++ src/lib/components/Icons.svelte
@@ -10,6 +10,11 @@
       svg: `<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline>`
     },
     {
+      name: 'Band',
+      viewBox: '0 0 24 24',
+      svg: `<circle cx="12" cy="12" r="10"></circle><line stroke-linecap="round" x1="12" y1="16" x2="12" y2="12"></line><line stroke-linecap="round" x1="12" y1="8" x2="12.01" y2="8"></line>`
+    },
+    {
       name: 'Sounds',
       viewBox: '0 0 24 24',
       svg: `<path d="M3 18v-6a9 9 0 0 1 18 0v6"></path><path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path>`
