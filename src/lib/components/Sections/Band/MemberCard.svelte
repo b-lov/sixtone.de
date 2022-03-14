81f9b6ec309d119ec4b921a695394f24cb7a4851
maxpiano <max@bilov.de>
2022-03-15 00:41:42 +0100

    added background spot on hover

--- src/lib/components/Sections/Band/MemberCard.svelte
+++ src/lib/components/Sections/Band/MemberCard.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-  import DotOverlay from '$lib/components/DotOverlay.svelte';
+  import { fly } from 'svelte/transition';
 
   export let member;
+
+  let background = false;
+
+  const randomFly = () => {
+    return (Math.random() - 0.5) * 2000;
+  };
 </script>
 
 <div class="flex flex-col items-center {$$props.class}">
-  <div class="relative">
-    <!-- <DotOverlay /> -->
+  <div
+    class="relative mb-3"
+    on:mouseenter={() => (background = true)}
+    on:mouseleave={() => (background = false)}
+  >
+    {#if background}
+      <div
+        in:fly={{ y: randomFly(), x: randomFly() }}
+        out:fly={{ y: randomFly(), x: randomFly() }}
+        class="absolute rounded-full w-full h-full top-0 bg-accent/40"
+      />
+    {/if}
     <img
-      src="images/band/{member.photo}.png"
+      src="images/band/third/{member.photo}.png"
       alt="{member.name} Photo"
-      class="w-[15rem] h-[15rem] object-cover object-top mb-3 shadow-lg rounded-full"
+      class="w-72 h-72 object-contain rounded-full object-top shadow-lg relative"
     />
   </div>
   <h1 class="font-oswald text-2xl text-neutral-200">{member.name}</h1>
