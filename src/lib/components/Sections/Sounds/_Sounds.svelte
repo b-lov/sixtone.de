306ca6b3a8d36f4754c783053b9ed5a41dae82b8
maxpiano <max@bilov.de>
2022-03-16 00:14:44 +0100

    use only global paths in tailwind square bracket notation

--- src/lib/components/Sections/Sounds/_Sounds.svelte
+++ src/lib/components/Sections/Sounds/_Sounds.svelte
@@ -3,9 +3,10 @@
   import tracks from './tracks';
 </script>
 
+<!-- style="background-image: url('images/sounds/music.jpg');" -->
 <div class="flex items-center justify-center px-16 max-w-5xl mx-auto">
   <div class="w-full grid grid-cols-5 grid-rows-1 gap-3">
-    <div class="col-span-2 bg-[url('images/sounds/music.jpg')] bg-cover bg-center" />
+    <div class="col-span-2 bg-[url('/static/images/sounds/music.jpg')] bg-cover bg-center" />
     <div class="col-span-3 flex flex-col gap-3">
       {#each tracks as { name, src }}
         <Player {name} {src} />
