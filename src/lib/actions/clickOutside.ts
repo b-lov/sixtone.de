47edfe2fc5ab5157c8699983bed80214b7af9fed
maxpiano <max@bilov.de>
2022-03-17 00:40:22 +0100

    added todo for clickoutside

--- src/lib/actions/clickOutside.ts
+++ src/lib/actions/clickOutside.ts
@@ -1,6 +1,7 @@
 export default (node: HTMLElement, onEventFunction: () => void): { destroy(): void } => {
   const handleClick = (event: PointerEvent) => {
     const path = event.composedPath();
+    // TODO: make exclusions dynamic
     if (!path.includes(node) && !path.includes(document.getElementById('Hamburger'))) {
       onEventFunction();
     }
