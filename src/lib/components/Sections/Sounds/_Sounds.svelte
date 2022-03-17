929080c1d012d7d6461506d509656b0486091098
maxpiano <max@bilov.de>
2022-03-17 14:52:24 +0100

    responsive sounds

--- src/lib/components/Sections/Sounds/_Sounds.svelte
+++ src/lib/components/Sections/Sounds/_Sounds.svelte
@@ -4,10 +4,12 @@
 </script>
 
 <!-- style="background-image: url('images/sounds/music.jpg');" -->
-<div class="flex items-center justify-center px-16 max-w-5xl mx-auto">
+<div class="flex items-center justify-center px-4 max-w-5xl mx-auto">
   <div class="w-full grid grid-cols-5 grid-rows-1 gap-3">
-    <div class="col-span-2 bg-[url('/static/images/sounds/music.jpg')] bg-cover bg-center" />
-    <div class="col-span-3 flex flex-col gap-3">
+    <div
+      class="hidden sm:block col-span-2 bg-[url('/static/images/sounds/music.jpg')] bg-cover bg-right"
+    />
+    <div class="col-span-5 sm:col-span-3 flex flex-col gap-3">
       {#each tracks as { name, src }}
         <Player {name} {src} />
       {/each}
