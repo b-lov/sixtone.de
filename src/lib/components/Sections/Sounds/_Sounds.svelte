358c838f2cfdfd803d8faac9c84f42910a1ee77e
maxpiano <max@bilov.de>
2022-04-09 23:44:57 +0200

    vite doesnt like absolute paths to static folder

--- src/lib/components/Sections/Sounds/_Sounds.svelte
+++ src/lib/components/Sections/Sounds/_Sounds.svelte
@@ -10,7 +10,7 @@
     <div class="w-full grid grid-cols-5 grid-rows-1 gap-3">
       <div
         class="hidden sm:block col-span-2 bg-cover bg-right"
-        style="background-image: url('images/sounds/soundspic.jpg');"
+        style="background-image: url('images/sounds/music.jpg');"
       />
       <div class="col-span-5 sm:col-span-3 flex flex-col gap-3">
         {#each tracks as { name, src }}
