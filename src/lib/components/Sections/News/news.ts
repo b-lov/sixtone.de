2642a8e82fdadb17e804f5403db66bd3bceec314
maxpiano <max@bilov.de>
2022-04-12 22:40:02 +0200

    make graphql data retrieval better Fixes #34

--- src/lib/components/Sections/News/news.ts
+++ src/lib/components/Sections/News/news.ts
@@ -4,20 +4,20 @@ export default [
     datum: '16. November 2021',
     author: 'V. Miller',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi id esse quo culpa cupiditate laboriosam officiis...',
-    picture: 'images/gallery/02.jpg'
+    picture: 'https://admin.sixtone.de/wp-content/uploads/14-768x512.jpg'
   },
   {
     title: 'Neuer Post',
     datum: '13. Januar 2022',
     author: 'Miller',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi id esse quo culpa cupiditate laboriosam officiis...',
-    picture: 'images/gallery/13.jpg'
+    picture: 'https://admin.sixtone.de/wp-content/uploads/08-768x512.jpg'
   },
   {
     title: 'Hallo Freunde',
     datum: '3. März 2021',
     author: 'Miller',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi id esse quo culpa cupiditate laboriosam officiis...',
-    picture: 'images/gallery/11.jpg'
+    picture: 'https://admin.sixtone.de/wp-content/uploads/01-768x485.jpg'
   }
 ];
