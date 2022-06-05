3ed1cdce5abb05e1a9f469ac4eb07cbcbd3417e7
maxpiano <max@bilov.de>
2022-06-05 22:43:11 +0200

    add language store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,6 +2,8 @@ import { writable } from 'svelte/store';
 
 export const currentSection = writable();
 
-export const navMenuOpen = writable(false);
+export const navMenuOpen = writable(true);
 
 export const bigHeader = writable();
+
+export const displayLanguage = writable('de');
