d1271b5996c0dd72cb51ab30928947aaf0b76950
maxpiano <max@bilov.de>
2022-06-05 22:41:02 +0200

    polylang Fixes #47

---
+++ src/lib/components/NavMenu/LanguagePicker.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Icons from '../Icons.svelte';
+
+  let expanded = false;
+</script>
+
+<div
+  class="flex p-4 gap-4 transition-transform {expanded ? 'translate-x-0' : 'translate-x-[104px]'}"
+>
+  <button on:click={() => (expanded = !expanded)}>
+    <Icons name="flag_de" size={2} />
+  </button>
+  <button on:click={() => (expanded = false)}>
+    <Icons name="flag_ru" size={2} />
+  </button>
+  <button on:click={() => (expanded = false)}>
+    <Icons name="flag_gb" size={2} />
+  </button>
+</div>
+
+<style lang="postcss">
+  button {
+    @apply flex rounded-full border-2 border-neutral-300 overflow-hidden;
+  }
+</style>
