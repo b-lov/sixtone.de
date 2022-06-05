c681aa3b20f2f74ba340bb24a3434f5bb3995a45
maxpiano <max@bilov.de>
2022-06-05 23:13:15 +0200

    use languages array and store to dynamically style picker

--- src/lib/components/NavMenu/LanguagePicker.svelte
+++ src/lib/components/NavMenu/LanguagePicker.svelte
@@ -1,21 +1,31 @@
 <script>
   import Icons from '../Icons.svelte';
+  import { currentLanguage } from '$lib/stores';
 
   let expanded = false;
+  const languages = ['de', 'ru', 'en'];
 </script>
 
 <div
   class="flex p-4 gap-4 transition-transform {expanded ? 'translate-x-0' : 'translate-x-[104px]'}"
 >
   <button on:click={() => (expanded = !expanded)}>
-    <Icons name="flag_de" size={2} />
+    {#key $currentLanguage}
+      <Icons name={`flag_${$currentLanguage}`} size={2} />
+    {/key}
   </button>
-  <button on:click={() => (expanded = false)}>
-    <Icons name="flag_ru" size={2} />
-  </button>
-  <button on:click={() => (expanded = false)}>
-    <Icons name="flag_gb" size={2} />
-  </button>
+  {#each languages as language}
+    {#if language !== $currentLanguage}
+      <button
+        on:click={() => {
+          currentLanguage.set(language);
+          expanded = false;
+        }}
+      >
+        <Icons name={`flag_${language}`} size={2} />
+      </button>
+    {/if}
+  {/each}
 </div>
 
 <style lang="postcss">
