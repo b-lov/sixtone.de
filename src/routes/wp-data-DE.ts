3799243c0728dae7a8f61429ec566deaa479a205
maxpiano <max@bilov.de>
2022-06-19 22:09:54 +0200

    polylang Fixes #47

---
+++ src/routes/wp-data-DE.ts
@@ -0,0 +1,25 @@
+import wordpressQuery from '$lib/utils/wp-query';
+const endpoint = import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL;
+
+export async function get(): Promise<Record<string, unknown>> {
+  const data = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query: wordpressQuery('DE') })
+  }).then((res) => res.json());
+
+  const { sections, members, sounds, images, feedbacks, partners } = data.data;
+
+  return {
+    body: {
+      sections,
+      members,
+      sounds,
+      images,
+      feedbacks,
+      partners
+    }
+  };
+}
