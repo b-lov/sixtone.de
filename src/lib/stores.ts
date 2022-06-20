dc43592a19920839bf67e047ec2606fce60e190d
maxpiano <max@bilov.de>
2022-06-20 10:39:22 +0200

    delete moved/unneeded files

--- src/lib/stores.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const currentSection = writable();
-
-export const navMenuOpen = writable(false);
-
-export const bigHeader = writable();
-
-export const currentLanguage = writable('RU');
