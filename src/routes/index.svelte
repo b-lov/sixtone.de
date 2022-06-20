328189f4519850866d9f7620e542e1f2c79d2a27
maxpiano <max@bilov.de>
2022-06-20 10:39:08 +0200

    testing wp store

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { getContext, onMount } from 'svelte';
+  import { getWpData } from '$lib/utils';
+  import { currentLocale, rawData, sectionsStore } from '$lib/stores/wp-store';
   import * as Sections from '$lib/components/Sections';
-  import { getContext } from 'svelte';
 
-  const sectionData: { [key: string]: {} } = getContext('sectionData');
+  const sectionData: { [key: string]: any } = getContext('sectionData');
 
   const {
     homeData,
@@ -14,13 +16,23 @@
     partnersData,
     contactData
   } = sectionData;
+
+  onMount(async () => {
+    getWpData($currentLocale, rawData);
+  });
 </script>
 
+{#each $sectionsStore as section}
+  {section.name}
+{/each}
 <Sections.Home {homeData} />
 <Sections.Band {bandData} />
 <Sections.Sounds {soundsData} />
 <Sections.Gallery {galleryData} />
-<Sections.News {newsData} />
+<!-- TODO: get insta token with new password -->
+{#if newsData.news}
+  <Sections.News {newsData} />
+{/if}
 <Sections.Feedback {feedbackData} />
 <Sections.Partners {partnersData} />
 <Sections.Contact {contactData} />
