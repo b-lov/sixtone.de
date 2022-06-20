09cd9e170bced4cc7d9c19fa92859dbdfdf0a1bf
maxpiano <max@bilov.de>
2022-06-20 10:38:46 +0200

    language picker get data

--- src/lib/components/NavMenu/LanguagePicker.svelte
+++ src/lib/components/NavMenu/LanguagePicker.svelte
@@ -1,6 +1,7 @@
 <script>
+  import { getWpData } from '$lib/utils';
+  import { rawData, currentLocale } from '$lib/stores/wp-store';
   import Icons from '../Icons.svelte';
-  import { currentLanguage } from '$lib/stores';
 
   let expanded = false;
   const languages = ['DE', 'RU', 'EN'];
@@ -10,16 +11,17 @@
   class="flex p-4 gap-4 transition-transform {expanded ? 'translate-x-0' : 'translate-x-[104px]'}"
 >
   <button on:click={() => (expanded = !expanded)}>
-    {#key $currentLanguage}
-      <Icons name={`flag_${$currentLanguage}`} size={2} />
+    {#key $currentLocale}
+      <Icons name={`flag_${$currentLocale}`} size={2} />
     {/key}
   </button>
   {#each languages as language}
-    {#if language !== $currentLanguage}
+    {#if language !== $currentLocale}
       <button
         on:click={() => {
-          currentLanguage.set(language);
+          currentLocale.set(language);
           expanded = false;
+          getWpData($currentLocale, rawData);
         }}
       >
         <Icons name={`flag_${language}`} size={2} />
