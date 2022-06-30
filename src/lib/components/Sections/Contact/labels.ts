ca633a35fb81e2375cf3bd3b2c2f5f3b4e2b6097
maxpiano <max@bilov.de>
2022-06-30 20:48:20 +0200

    localization of contact form labels

---
+++ src/lib/components/Sections/Contact/labels.ts
@@ -0,0 +1,29 @@
+export default {
+  DE: {
+    name: 'Name',
+    email: 'E-mail',
+    tel: 'Tel (optional)',
+    message: 'Nachricht',
+    privacy:
+      'Ich stimme der Verarbeitung meiner Daten zum Zweck der Kontaktaufnahme durch Sixtone zu und akzeptiere den Datenschutz.',
+    sendButton: 'Senden'
+  },
+  EN: {
+    name: 'Name',
+    email: 'Email',
+    tel: 'Tel (optional)',
+    message: 'Message',
+    privacy:
+      'I accept the processing of my data for the purpose of contact with Sixtone and accept the privacy policy.',
+    sendButton: 'Send'
+  },
+  RU: {
+    name: 'Имя',
+    email: 'Email',
+    tel: 'Тел (опционально)',
+    message: 'Сообщение',
+    privacy:
+      'Я согласен на обработку моих данных с целью связи со мной компанией Sixtone и принимаю политику конфиденциальности.',
+    sendButton: 'Отправить'
+  }
+};
